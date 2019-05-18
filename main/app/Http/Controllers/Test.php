e5ea777b0ab05febeb82c42ad7443fa81e764b76
wangrunzemain <wangrunzemain@gmail.com>
2019-05-18 16:16:36 +0800

    Create TestController.php

---
+++ main/app/Http/Controllers/Test.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Conttollers\Controller;
+use Redis;
+
+class Test extends Controller
+{
+  public function redisTest ()
+  {
+    Redis::set('redisTest',1);
+    $result = Redis::get('redisTest');
+    if ($result == 1)
+    {
+      Redis::set('redisTest',0);
+      return 'Read : succeed <br />Write : succeed';
+    }
+    else
+    {
+      return 'Read : failed <br />Write : failed';
+    }
+  }
+}
