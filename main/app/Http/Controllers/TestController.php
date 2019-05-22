abd82ee893b21b80ed4122d80a50bc077b8e39d2
wangrunzemain <wangrunzemain@gmail.com>
2019-05-22 21:50:19 +0800

    Update test mysql function

--- main/app/Http/Controllers/TestController.php
+++ main/app/Http/Controllers/TestController.php
@@ -38,7 +38,29 @@ class TestController extends Controller
     foreach($before_unit as $before_each)
     {
       echo "$before_each";
-      $before = $before_each;
+      $before = $before_each->target;
+    }
+    DB::update('update TestForm set target = 1 where target = ?',[0]);
+    $result_unit = DB::select('select * from TestForm where target = ?',[1]);
+        foreach($result_unit as $result_each)
+        {
+          $result = $result_each->target;
+        }
+    if ($before == 0)
+    {
+      echo 'Read : succeed <br />';
+    }
+    else
+    {
+      echo 'Read : failed <br />';
+     }
+    if ($result == 1)
+    {
+      return 'Write : succeed';
+    }
+    else
+    {
+      return 'Write : failed';
     }
   }
 }
