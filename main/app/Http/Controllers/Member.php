d1d71a89f5ecf295303e6895af44d951e18586da
wangrunzemain <wangrunzemain@gmail.com>
2019-05-26 01:12:39 +0800

    merge

---
+++ main/app/Http/Controllers/Member.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controller;
+
+class Member extends Controllers
+{
+  public function test ()
+  {
+    return "main";
+  }
+}
---
+++ main/app/Http/Controllers/Test.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+class test extends Controller
+{
+  public function hello(){
+    echo "helloworld";
+  }
+}
